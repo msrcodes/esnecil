86f67b0461ea7437e2d73499ea4fd4901310843e
Mikael Rozee <mikaelsrozee@gmail.com>
2021-10-13 13:38:28 +0100

    ci(release): fix commit message getting blocked

--- release.config.js
+++ release.config.js
@@ -13,6 +13,8 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['CHANGELOG.md'],
+        message:
+          'ci(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/npm',
